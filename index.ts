6a2dbe5dfbc5059407b5ac9e549f3caa6ee86b03
ImmortalLark <fanfanyayu@gmail.com>
2018-03-02 11:02:40 +0800

    1.完善构建选项 2.重构nestController代码

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
 import { Controller } from "@nestjs/common";
 import { PATH_METADATA } from '@nestjs/common/constants';
 
-export default function (path, ...TargetClass) {
-  return TargetClass.map((Target, index) => {
-    let newPath
-    let oldPath = Reflect.getMetadata(PATH_METADATA, Target);
-
-    if(oldPath) {
-      newPath = `${path}/${oldPath}`
-    }
-    
-    @Controller(newPath)
-    class NestController extends Target {}
-
-    let controller = NestController;
-    Reflect.defineProperty(NestController, "name", { value: `NestController${index}`, writable: true });
-
-    return controller;
-  })
+export default class NestRouter {
+  public static nestController (path: String, ...TargetClass: any[]): any[] {
+    return TargetClass.map((Target, index) => {
+      let newPath
+      let oldPath = Reflect.getMetadata(PATH_METADATA, Target);
+  
+      if(oldPath) {
+        newPath = `${path}/${oldPath}`
+      }
+      
+      @Controller(newPath)
+      class NestController extends Target {}
+  
+      let controller = NestController;
+      Reflect.defineProperty(NestController, "name", { value: `NestController${index}`, writable: true });
+  
+      return controller;
+    })
+  }
 }
