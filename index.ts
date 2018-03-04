fe331aded2cbcaaf25d06916f45dec7ba71b8b14
丰凡 <frorice@fengfandeMacBook-Pro.local>
2018-03-05 00:10:18 +0800

    # api 调整 1. 控制器路由数据结构修改为对象，可嵌套多层路由 2. 增加方法flattenRoutes用于生成扁平的控制器列表 ‘

--- index.ts
+++ index.ts
@@ -1,23 +1,27 @@
-import { Controller } from "@nestjs/common";
+import "reflect-metadata";
 import { PATH_METADATA } from '@nestjs/common/constants';
 
 export default class NestRouter {
-  public static nestController (path: String, ...TargetClass: any[]): any[] {
-    return TargetClass.map((Target, index) => {
-      let newPath
-      let oldPath = Reflect.getMetadata(PATH_METADATA, Target);
-  
-      if(oldPath) {
-        newPath = `${path}/${oldPath}`
+  public static nestController (path: String, routes: Object): any[] {
+      return NestRouter.flattenRoutes(path, routes);
+  }
+  // 嵌套控制器
+  private static flattenRoutes (rootPath, routes: Object): Function[] {
+    // 拍平路由树
+    let flatRoutes = [];
+    Object.keys(routes).forEach((path) => {
+      if ( routes[path] instanceof Function) {
+        let oldPath = Reflect.getMetadata(PATH_METADATA, routes[path]);
+        // 新类继承自原controller 避免污染
+        class NestedController extends routes[path] {}
+        // 修改路径
+        Reflect.defineMetadata(PATH_METADATA, `${rootPath}/${oldPath}`, NestedController);
+        // 使用原controller名重命名新controller
+        Reflect.defineProperty(NestedController, "name", { value: path, writable: true });
+        flatRoutes.push(NestedController);
       }
-      
-      @Controller(newPath)
-      class NestController extends Target {}
-  
-      let controller = NestController;
-      Reflect.defineProperty(NestController, "name", { value: `NestController${index}`, writable: true });
-  
-      return controller;
-    })
+      flatRoutes = flatRoutes.concat(NestRouter.flattenRoutes(`${rootPath}/${path}`, routes[path]));
+    });
+    return flatRoutes;
   }
 }
